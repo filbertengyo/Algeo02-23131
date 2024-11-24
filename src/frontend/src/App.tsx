020ac105eb35c455bef150b7beaa80d450b6fe6c
Arlow5761 <arlow5761@gmail.com>
2024-11-24 12:05:20 +0700

    chore: setup frontend

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Hello World!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
